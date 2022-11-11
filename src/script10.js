4f7efb1b5bf2a3f433905a9159aded9a2cff9f69
MinusD <lukovnikov50@mail.ru>
2022-11-11 20:14:59 +0300

    Added practice 8 (Captcha)

--- src/script10.js
+++ src/script10.js
@@ -1,63 +1,69 @@
-let captchaBlock = document.getElementById('captcha');
-let captchaInput = document.getElementById('captcha-input');
-let captchaButton = document.getElementById('captcha-button');
-
 class Captcha {
-    constructor(param) {
-
-    }
-
-}
-
-// let captcha = new Captcha({
-//     length: 5,
-//
-// }
+    constructor(length) {
+        this.failed = 0;
+        this.length = length;
+        this.captcha = document.getElementById('captcha');
+        this.captchaInput = document.getElementById('captcha-input');
+        this.captchaButton = document.getElementById('captcha-button');
+        this.captchaOutput = document.getElementById('captcha-output');
 
 
-let captcha = {
+        // Случайнай генирация больших и маленьких букв
+        this.captchaText = '';
+        let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-    fails: 0,
-    captchaBlock: document.getElementById('captcha'),
-    captchaInput: document.getElementById('captcha-input'),
-    captchaButton: document.getElementById('captcha-button'),
-    captchaSpan: document.getElementById('captcha-output'),
-    correctCaptcha: '',
-
-    init() {
-        this.fails = 0;
-        // this.captchaBlock = document.getElementById('captcha');
-        // this.captchaInput = document.getElementById('captcha-input');
-        // this.captchaButton = document.getElementById('captcha-button');
-        // this.captchaSpan = document.getElementById('captcha-output');
-
-        console.log(this.captchaInput);
-
-        // Генерация случайной строки
-        let randomString = '';
-        let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-        for (let i = 0; i < 5; i++) {
-            randomString += possible.charAt(Math.floor(Math.random() * possible.length));
+        for (let i = 0; i < this.length; i++) {
+            this.captchaText += letters.charAt(Math.floor(Math.random() * letters.length));
         }
+        this.captchaOutput.innerHTML = this.captchaText;
 
-        this.correctCaptcha = randomString;
-        this.captchaSpan.innerHTML = randomString;
-
-        this.captchaButton.addEventListener('click', this.checkCaptcha);
-    },
+        this.captchaButton.addEventListener('click', this.checkCaptcha.bind(this));
+    }
 
     checkCaptcha() {
-        console.log(this.captchaInput);
-
-        // if (this.captchaInput.value === this.correctCaptcha) {
-        //     this.captchaBlock.style.display = 'none';
-        //     this.failds = 1;
-        // } else {
-        //     this.captchaInput.value = '';
-        // }
+        if (this.failed === 0) {
+            if (this.captchaInput.value.trim() === this.captchaText) {
+                this.hideCaptcha();
+            } else {
+                this.failed = 1;
+                this.captchaInput.value = '';
+                this.captchaInput.placeholder = 'Неверно';
+                this.captchaInput.style.borderColor = 'red';
+                this.captchaButton.innerHTML = 'Попробовать еще раз';
+                this.generateCaptchaV2();
+            }
+        } else {
+            if (this.captchaInput.value.trim() === this.captchaText.toString()) {
+                this.hideCaptcha();
+            } else {
+                this.failed++;
+                this.generateCaptchaV2();
+            }
+        }
     }
-};
 
-cap = captcha;
-cap.init();
+    generateCaptchaV2() {
+        // Генерируем 2 случайных числа и операцию
+
+        let num1 = Math.floor(Math.random() * 10);
+        let num2 = Math.floor(Math.random() * 10);
+        let operation = Math.floor(Math.random() * 2);
+
+        if (operation === 0) {
+            this.captchaText = num1 + num2;
+            this.captchaOutput.innerHTML = num1 + ' + ' + num2 + ' = ' + this.captchaText;
+        } else {
+            this.captchaText = num1 - num2;
+            this.captchaOutput.innerHTML = num1 + ' - ' + num2;
+        }
+    }
+
+    hideCaptcha() {
+        this.captcha.style.display = 'none';
+        let btn = document.getElementById('btn-for-captcha');
+        btn.disabled = false;
+    }
+}
+
+let captcha = new Captcha(8);
 
