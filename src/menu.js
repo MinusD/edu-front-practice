38bb6683e51e18bf5c1a07d1e762c5c07b0ad644
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-11-18 17:58:15 +0300

    FULL REFRACTOR

---
+++ src/menu.js
@@ -0,0 +1,30 @@
+const insertMenu = () => {
+    let div = document.createElement('div');
+    document.body.insertAdjacentHTML("afterbegin", `<div id="fixed-menu-block">
+    <ul>
+        <li><a href="./index1.html">Практическая №1</a></li>
+        <li><a href="./index2.html">Практическая №2</a></li>
+        <li><a href="./index3.html">Практическая №3</a></li>
+        <li><a href="./index4.html">Практическая №4</a></li>
+        <li><a href="./index5.html">Практическая №5</a></li>
+        <li><a href="./index6.html">Практическая №6</a></li>
+        <li><a href="./index7.html">Практическая №7</a></li>
+        <li><a href="./index8.html">Практическая №8</a></li>
+        <li><a href="./index9.html">Практическая №9</a></li>
+        <li><a href="./index10.html">Практическая №10</a></li>
+    </ul>
+  </div>`);
+
+    div = document.getElementById('fixed-menu-block');
+    div.style.position = 'fixed';
+    div.style.top = '0';
+    div.style.right = '0';
+    div.style.backgroundColor = '#2a2a2a';
+    div.style.zIndex = '1000';
+    div.style.padding = '10px';
+    div.style.borderRadius = '0 0 0 10px';
+    div.style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, 0.5)';
+    div.style.color = '#fff';
+}
+
+insertMenu();
